37a51b11f35d9f4133cc0f803dc7b3938460f48c
adeolammy <adeolammy2020@gmail.com>
2022-11-01 10:52:44 +0100

    init the Subcription panel to the project

--- src/Component/subscription.js
+++ src/Component/subscription.js
@@ -1,32 +1,90 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { json } from 'react-router-dom';
 
-class Subscribe extends Component{
-    constructor(props){
+class subscription extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            email: ''
+            email: '',
+            error: false,
+            success: false
         }
-
     }
-    render(){
-        return(
-            <div className='subcribe_panel'>
-                <h3>Subscribe to us</h3>
+
+    saveSubscription = (email) => {
+        const URL_EMAIL =   'http://localhost:3004/subcriptions'
+
+        fetch(URL_EMAIL, {
+            method: 'post',
+            headers: {
+                'Accept' : 'application/json',
+                'Content-Type' :'application/json'
+            },
+            body: JSON.stringify({email})
+        }).then(res=>res.json())
+        .then(()=>{
+            this.setState({
+                email: '' , 
+                success: true
+            })
+        })
+    }
+
+    clearMessage = () =>{
+        setTimeout( function(){
+            this.setState({
+                error:false,
+                success:false
+            })
+        }.bind(this),3000)
+    }
+
+
+ 
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        let email = this.state.email
+        let regex = /\S+@\S+\.\S+/;
+
+        if(regex.test(email)){
+            this.saveSubscription(email)
+        }else{
+                this.setState({error:true})
+        }
+        this.clearMessage()
+    }
+
+
+    onchangeInput = (e) =>{
+        this.setState({
+            email:e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className='subscribe_panel'>
+                <h3>Subcribe to us</h3>
                 <div>
-                    <form>
-                        <input type="text"/>
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                        placeholder ='email@yourEmail.com' 
+                        type="text"
+                        value={this.state.email}
+                        onChange={this.onchangeInput}
+                        />
+                        <div className={this.state.error ? "error show" : "error"}>chech your email</div>
+                        <div className={this.state.success ? "success show" : "success"}>Thank you</div>
                     </form>
                 </div>
-
                 <small>
-                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae similique ducimus numquam 
-                       iusto doloribus ipsam voluptatem magnam excepturi! 
-                       Ipsa reprehenderit eum tenetur illum soluta quas rerum pariatur provident magnam nostrum?
+                    Lorem ipsum, dolor sit amet consectetur 
+                    adipisicing elit. Neque eaque et minus iusto
+                    dolore tempore earum corporis magni quasi ipsam?
                 </small>
-
             </div>
-        )
+        );
     }
 }
 
-export default Subscribe;
+export default subscription;
